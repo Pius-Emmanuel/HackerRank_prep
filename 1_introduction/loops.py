0ebc98b6255c5c12a4087f80cd54be16a8f584e9
Pius <piusemmanuelchukwuemeka@gmail.com>
2023-04-18 15:57:49 +0100

    add loops.py

---
+++ 1_introduction/loops.py
@@ -0,0 +1,13 @@
+"""for loop"""
+if __name__ == "__main__":
+    n = int(input("input n: "))
+    for i in range(0,n):
+        print(i**2)
+
+"""while loop"""
+if __name__ == "__main__":
+    x = int(input("input x: "))
+    y = 0
+    while y < x:
+        print(y**2)
+        y += 1
